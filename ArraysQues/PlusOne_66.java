06b3f0fbef71740630ef6c3e623c95a01c05d459
Ashhish-h <kushwaha.ashish2611@gmail.com>
2023-09-19 18:43:00 +0530

    added new ques

---
+++ ArraysQues/PlusOne_66.java
@@ -0,0 +1,15 @@
+class PlusOne_66{
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+	if (digits[i] < 9) {
+		digits[i]++;
+		return digits;
+	}
+	digits[i] = 0;
+    }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
