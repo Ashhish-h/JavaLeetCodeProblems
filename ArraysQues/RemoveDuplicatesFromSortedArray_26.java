5000cae9226a45d2814118b0fc9702aad752ce64
Ashhish-h <kushwaha.ashish2611@gmail.com>
2023-11-02 13:48:27 +0530

    Added Questions

---
+++ ArraysQues/RemoveDuplicatesFromSortedArray_26.java
@@ -0,0 +1,13 @@
+package ArrayQues;
+class RemoveDuplicatesFromSortedArray_26{
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
