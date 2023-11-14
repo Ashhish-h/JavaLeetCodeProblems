b8baf9f23e1be8ae6d7b9f1e515ce847b11d84ec
Ashhish-h <kushwaha.ashish2611@gmail.com>
2023-11-14 17:14:22 +0530

    Added ques

---
+++ Math/ClimbingStairs_70.java
@@ -0,0 +1,18 @@
+package Math;
+
+public class ClimbingStairs_70 {
+    public int climbStairs(int n) {
+        if(n ==0 || n == 1){
+            return 1;
+        }
+
+        int prev = 1, current = 1;
+        for(int i = 2; i <= n; i++){
+            int temp = current;
+            current = prev + current;
+            prev = temp;
+        }
+        return current;
+    }s  
+}
+
