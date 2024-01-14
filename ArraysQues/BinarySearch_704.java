e0ab575891b4968a6fc565bf6f6ad2e8ab24cdbf
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-01-14 17:50:09 +0530

    Added BinarySearch_704

---
+++ ArraysQues/BinarySearch_704.java
@@ -0,0 +1,21 @@
+package ArraysQues;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int middle = start + (end - start) / 2;
+            if(nums[middle] == target){
+                return middle;
+            }
+
+            if(nums[middle] < target){
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
