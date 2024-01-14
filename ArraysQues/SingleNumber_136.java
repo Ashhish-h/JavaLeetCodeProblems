bf295cdba10a13d294d616c8b9437f4e91fcc27f
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-01-14 17:44:08 +0530

    Added SingleNumber_136

---
+++ ArraysQues/SingleNumber_136.java
@@ -0,0 +1,11 @@
+package ArraysQues;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = nums[0];
+        for(int i = 1; i < nums.length; i++){
+          xor ^= nums[i];  
+        }
+        return xor;
+    }
+}
