e7b0d945cc7cd4f12afa2e6bf29f03c8e564abd2
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-03-06 11:03:12 +0530

    create Linkedlist directory for linkedlist questions add RemoveDuplicatesFromSortedList

---
+++ LinkedList/RemoveDuplicatesFromSortedList_83.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        if(node == null){
+            return node;
+        }
+        while(node.next != null){
+            if(node.val == node.next.val){
+                node.next = node.next.next;
+            } else{
+                node = node.next;
+            }
+        }
+        return head;
+        
+    }
+}
