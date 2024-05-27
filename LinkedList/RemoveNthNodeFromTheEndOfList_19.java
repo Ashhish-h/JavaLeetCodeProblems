bf3ab1578a4741b30e6d2a26b3163fcf9c67c26a
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-28 01:08:46 +0530

    add problem RemoveNthNodeFromTheEndOfList_19 from leetCode

---
+++ LinkedList/RemoveNthNodeFromTheEndOfList_19.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class RemoveNthNodeFromTheEndOfList_19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) fast = fast.next;
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+s
