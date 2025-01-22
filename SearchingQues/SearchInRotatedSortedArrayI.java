7a845c468bec8e371c07c6fe440f27cb75a71127
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-01-22 22:07:31 +0530

    add: SearchInRotatedSortedArrayI in SearchingQues

---
+++ SearchingQues/SearchInRotatedSortedArrayI.java
@@ -0,0 +1,28 @@
+package SearchingQues;
+
+public class SearchInRotatedSortedArrayI {
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target)
+                return mid;
+
+            // left sorted part
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // search in right part
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
