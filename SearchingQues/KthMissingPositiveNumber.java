a663516e41c36303ca0eb71ad85bd329068aa9cf
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-01-31 20:23:42 +0530

    add: KthMissingPositiveNumber

---
+++ SearchingQues/KthMissingPositiveNumber.java
@@ -0,0 +1,17 @@
+package SearchingQues;
+
+public class KthMissingPositiveNumber {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
+    }
+}
